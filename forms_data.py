cd5d59239e420e057db5aaf087872e672fd6e167
abelespinromero <116482938+abelespinromero@users.noreply.github.com>
2023-10-26 23:40:23 +0200

    upload data file

---
+++ forms_data.py
@@ -0,0 +1,20 @@
+#Scrape emails
+EMAILS=[" ",] #MODIFY THAT VARIABLE WITH YOURS EMAILS WITH THE NOTICES. Ex: EMAILS=["address1.gmail.com", "address2.gmail.com"]
+APP_PASSWORDS=[" ",] #MODIFY THAT VARIABLE WITH YOURS APP PASSWORDS OF EMAILS. Ex: APP_PASSWORDS=["xxxx xxxx xxxx xxxx", "xxxx xxxx xxxx xxxx"]
+
+#Forms data
+URL = "https://support.google.com/legal/contact/lr_counternotice?product=websearch&utm_source=wnc_594600&utm_medium=gamma&utm_campaign=wnc_594600&utm_content=msg_638303&hl=en"
+COUNTRY=" "
+NAME=" "
+TITLE=" "
+COMPANY=" "
+ADDRESS=" "
+FORM_EMAIL=" "
+PHONE_NUMBER=" "
+DETAILS=""
+SIGNATURE=" "
+
+
+#Captcha Solver
+API_CAPTCHA_KEY=" "
+SITE_CAPTCHA_KEY=" "
